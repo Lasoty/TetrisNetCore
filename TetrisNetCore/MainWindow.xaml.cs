63abdaaf31656f42d30214be5e6efef5cefa90eb
Leszek Lewandowski <lasoty@o2.pl>
2021-09-17 23:33:48 +0200

    Aplikacja bazowa.

---
+++ TetrisNetCore/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using TetrisNetCore.Extensions;
+
+namespace TetrisNetCore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml - Logika interakcji
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Utwórz instancję.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
