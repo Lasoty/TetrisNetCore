7399b6b868bc141c3c0df0fff31fc63489d478de
Leszek Lewandowski <lasoty@o2.pl>
2021-09-18 12:52:01 +0200

    Wyświetlanie komórek

--- TetrisNetCore/MainWindow.xaml.cs
+++ TetrisNetCore/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using TetrisNetCore.Extensions;
+using TetrisNetCore.ViewModels;
 
 namespace TetrisNetCore
 {
@@ -12,12 +13,67 @@ namespace TetrisNetCore
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Właściwości
+        
+        public GameViewModel Game
+        {
+            get { return this.DataContext as GameViewModel; }
+            set { this.DataContext = value; }
+        }
+
+        #endregion
+
+        #region Konstruktor
+        
         /// <summary>
         /// Utwórz instancję.
         /// </summary>
         public MainWindow()
         {
+            this.Game = new GameViewModel();
             this.InitializeComponent();
+        } 
+
+        #endregion
+
+        #region Inicjalizacja
+
+        private void SetupField(Grid field, CellViewModel[,] cells, byte blockSize)
+        {
+            for (int r = 0; r < cells.GetLength(0); r++)
+            {
+                field.RowDefinitions.Add(new RowDefinition()
+                {
+                    Height = new GridLength(blockSize, GridUnitType.Pixel)
+                }) ;
+            }
+
+            for (int c = 0; c < cells.GetLength(1); c++)
+            {
+                field.ColumnDefinitions.Add(new ColumnDefinition() 
+                {
+                    Width = new GridLength(blockSize, GridUnitType.Pixel)
+                });
+            }
+
+            foreach (IndexedItem2<CellViewModel> item in cells.WithIndex())
+            {
+                SolidColorBrush brush = new SolidColorBrush();
+                TextBlock control = new TextBlock
+                {
+                    DataContext = item.Element,
+                    Background = brush,
+                    Margin = new Thickness(1)
+                };
+
+                BindingOperations.SetBinding(brush, SolidColorBrush.ColorProperty, new Binding("Color.Value"));
+
+                Grid.SetRow(control, item.X);
+                Grid.SetColumn(control, item.Y);
+                field.Children.Add(control);
+            }
         }
+
+        #endregion
     }
 }
---
+++ TetrisNetCore/ViewModels/CellViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media;
+
+namespace TetrisNetCore.ViewModels
+{
+    public class CellViewModel
+    {
+        public Color Color { get; set; }
+    }
+}
